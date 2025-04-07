72b944a09bbdb19c87ab7eaed5da045d2eaca843
SWOMH <tifame67@mail.ru>
2025-04-07 17:14:49 +0300

    upd constants

--- src/utils/api-constant.js
+++ src/utils/api-constant.js
@@ -1,6 +1,24 @@
-export const API_URL_HOST = 'http://localhost:8000';
-export const API_URL_LOGIN = `${API_URL_HOST}/user`;
-export const API_USER_REGISTER = `${API_URL_HOST}/user/register`;
-export const API_LUNCH_ALL = `${API_URL_HOST}/lunch`;
-export const API_URL_ORDER = `${API_URL_HOST}/ordering_food`;
-export const API_URL_ORDER_HISTORY = `${API_URL_HOST}/ordering_food/history`;
+export const API_URL_HOST = 'http://localhost:80';
+
+// USER
+export const API_USER_GET_INFO = `${API_URL_HOST}/user`;
+export const API_USER_REGISTER = `${API_USER_GET_INFO}/register`;
+export const API_USER_ALL_ORDERS = `${API_USER_GET_INFO}/orders`;
+export const API_USER_ACTUAL_ORDERS = `${API_USER_ALL_ORDERS}/actual`;
+
+// DISH
+export const API_GET_ALL_DISH = `${API_URL_HOST}/dish`
+
+
+// ORDERS
+export const API_URL_ORDER = `${API_URL_HOST}/order`;
+export const API_URL_ORDER_HISTORY = `${API_URL_HOST}/history`;
+
+
+// ADMIN
+export const API_ADMIN_GET_ALL_USERS = `${API_URL_HOST}/user/get_users`
+export const API_ADMIN_EDIT_USER_STATUS = `${API_URL_HOST}/user/edit_status`
+export const API_ADMIN_URL_ALL_ORDERS = `${API_URL_HOST}/order/actual_orders`
+export const API_ADMIN_URL_EDIT_ORDER = `${API_URL_HOST}/order/edit_status`
+export const API_ADMIN_ADD_DISH = `${API_URL_HOST}/dish/add`
+export const API_ADMIN_EDIT_DISH = `${API_URL_HOST}/dish/` // put запрос в url передать id dish
