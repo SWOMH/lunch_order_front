259df185384418103c432fe812fad545a165f57d
SWOMH <tifame67@mail.ru>
2025-04-11 23:02:28 +0300

    babel conf

---
+++ babel.config.js
@@ -0,0 +1,8 @@
+// babel.config.js
+module.exports = {
+    presets: [
+      "@babel/preset-env", // для поддержки современного JavaScript
+      "@babel/preset-react", // если вы используете React
+      "@babel/preset-typescript", // для поддержки TypeScript
+    ],
+  };
