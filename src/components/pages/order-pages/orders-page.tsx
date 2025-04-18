d52f03f9cf601d4707220af7838c8d925cd3ebbf
SWOMH <tifame67@mail.ru>
2025-04-18 17:28:00 +0300

    ref order page

--- src/components/pages/order-pages/orders-page.tsx
+++ src/components/pages/order-pages/orders-page.tsx
@@ -1,19 +1,49 @@
+import { useEffect } from 'react';
 import './orders-page.css';
+import { useAppDispatch, useAppSelector } from '../../../store/hooks';
+import { getUserOrders } from '../../../store/slices/orderingSlice';
+import { selectOrderHistoryError, selectOrderHistoryLoading, selectOrderUserHistory } from '../../../store/selectors/order/orderSelectors';
+import { NullOrdersPage } from './null-orders/null-orders';
+import Loader from '../../ui/Loader';
+import { OrderHistoryCard } from './order-card/order-history-card';
 
 export const OrdersPage = () => {
-  return (
-    <div className="orders-page">
-      <h1 className="page-title">Мои заказы</h1>
-      
-      <div className="orders-empty-state">
-        <div className="orders-icon">
-          <svg width="64" height="64" viewBox="0 0 24 24" fill="#ccc" xmlns="http://www.w3.org/2000/svg">
-            <path d="M19 3H14.82C14.4 1.84 13.3 1 12 1C10.7 1 9.6 1.84 9.18 3H5C3.9 3 3 3.9 3 5V19C3 20.1 3.9 21 5 21H19C20.1 21 21 20.1 21 19V5C21 3.9 20.1 3 19 3ZM12 3C12.55 3 13 3.45 13 4C13 4.55 12.55 5 12 5C11.45 5 11 4.55 11 4C11 3.45 11.45 3 12 3ZM14 17H7V15H14V17ZM17 13H7V11H17V13ZM17 9H7V7H17V9Z" />
-          </svg>
-        </div>
-        <p className="empty-text">У вас пока нет заказов</p>
-        <p className="empty-desc">Здесь будут отображаться ваши заказы</p>
+  const dispatch = useAppDispatch();
+  const orders = useAppSelector(selectOrderUserHistory);
+  const isLoading = useAppSelector(selectOrderHistoryLoading);
+  const errorLoad = useAppSelector(selectOrderHistoryError);
+
+
+  useEffect(() => {
+    dispatch(getUserOrders('2')) // Для тестов пока прокину хардкодом
+  }, [dispatch]);
+  
+  if (isLoading) {
+    return <Loader />;
+  };
+  
+  if (errorLoad) {
+    return (
+      <div className="error-container">
+        <h2>Ошибка загрузки заказов</h2>
+        <p>'Произошла ошибка при загрузке заказов'</p>
       </div>
-    </div>
+    );
+  };
+
+  if (orders.length === 0) {
+    return (
+      <NullOrdersPage />
+    );
+  };
+
+  return (
+    <div className='order-list'>
+      {orders.map(order => (
+        <div className='order-item' key={order.order_id}>
+          <OrderHistoryCard order={order} />
+        </div>
+      ))}
+    </div>    
   );
 }; 
