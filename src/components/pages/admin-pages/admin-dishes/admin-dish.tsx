8f2e04ab8a22354a105422a1a70a0d0ab6f4eff1
SWOMH <tifame67@mail.ru>
2025-04-22 22:05:59 +0300

    ref admin el

--- src/components/pages/admin-pages/admin-dishes/admin-dish.tsx
+++ src/components/pages/admin-pages/admin-dishes/admin-dish.tsx
@@ -1,8 +1,12 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import style from './admin-dish.module.css';
 import { useAppSelector } from "../../../../store/hooks";
 import { selectAllDishes } from "../../../../store/selectors/dish/dishSelectors";
-import { Button } from "antd";
+import { Button, Modal } from "antd";
+import { IDish } from "../../../../types/dish-types";
+import DishDetails from "../../dish-pages/dish-detail/dish-detail";
+import { AdminDishCard } from "./admin-dish-card/admin-dish-card";
+
 
 
 export const AdminDishPage = () => {
@@ -10,26 +14,10 @@ export const AdminDishPage = () => {
 
 
     return (
-        <div className={style.admin_dish_page}>
+        <div className={style.admin_dish_page}>          
           <h1 className={style.page_dish_title}>Админ Меню</h1>
-          {dishes.length > 0 && dishes.map(dish => (
-            <div key={dish.id} className={style.cart_item}>
-              <div className={style.cart_item_image}>
-                <img 
-                  src={dish.image || 'https://via.placeholder.com/80?text=Нет+фото'} 
-                  alt={dish.name} 
-                />
-              </div>
-              <div className={style.cart_item_content}>
-                <h3 className={style.cart_item_name}>{dish.name}</h3>
-                <span className={style.cart_item_desc}>{dish.description}</span>                            
-                <p className={style.cart_item_price}>{dish.price} ₽</p>
-              </div>
-              <div className={style.cart_item_actions}>
-                <Button className={style.cart_button} onClick={() => {}}>−</Button>                                                    
-                <Button className={style.cart_button} type='primary' onClick={() => {}}>+</Button>
-              </div>
-            </div>
+          {dishes.length > 0 && dishes.map(dish => (            
+            <AdminDishCard dish={dish}/>
           ))}
         </div>
       )
