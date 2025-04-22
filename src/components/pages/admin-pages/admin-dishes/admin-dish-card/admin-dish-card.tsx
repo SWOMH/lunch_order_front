33b277f94e0b563113736542a187766b1ee08a72
SWOMH <tifame67@mail.ru>
2025-04-22 22:04:23 +0300

    admin card dish

---
+++ src/components/pages/admin-pages/admin-dishes/admin-dish-card/admin-dish-card.tsx
@@ -0,0 +1,67 @@
+import React, { useState } from "react";
+import style from './admin-dish-card.module.css';
+import { Button, Modal } from "antd";
+import { IDish } from "../../../../../types/dish-types";
+import DishDetails from "../../../dish-pages/dish-detail/dish-detail";
+
+
+interface IDishCardProps {
+    dish: IDish;
+};
+
+interface IDishEditButtonProps {
+    setIsModalOpen: (value: boolean) => void;
+};
+  
+  const DishEditButton: React.FC<IDishEditButtonProps> = ({ setIsModalOpen }) => {
+    const handleOpenModalClick = () => {
+      setIsModalOpen(true);
+    };
+  
+    return (
+      <div className={style.edit_buttons}>
+        <Button className={style.cart_button} onClick={() => {}}>Убрать из меню</Button>
+        <Button type="primary" onClick={handleOpenModalClick}>Изменить</Button>
+      </div>
+    );
+  }
+
+
+
+export const AdminDishCard: React.FC<IDishCardProps> = ({ dish }) => {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+
+    const handleCloseModalClick = () => {
+        setIsModalOpen(false);
+    };
+    
+    return (
+        <div key={dish.id} className={style.cart_item}>
+              <Modal title="Подробности"
+                footer={
+                  <Button type='primary'
+                  onClick={handleCloseModalClick}>Закрыть</Button>
+                }
+                open={isModalOpen}>
+                  <DishDetails dish={dish}></DishDetails>
+              </Modal>
+              <div className={style.cart_item_image}>
+                <img 
+                  src={dish.image || 'https://via.placeholder.com/80?text=Нет+фото'} 
+                  alt={dish.name} 
+                />
+              </div>
+              <div className={style.cart_item_content}>
+                <h3 className={style.cart_item_name}>{dish.name}</h3>
+                <span className={style.cart_item_desc}>{dish.description}</span>                            
+                <p className={style.cart_item_price}>{dish.price} ₽</p>
+              </div>
+              <div className={style.cart_item_actions}>                                                           
+                { !dish.available ? 
+                <Button className={style.cart_button_green} type="primary">Вернуть в меню</Button>
+                : <DishEditButton setIsModalOpen={setIsModalOpen}/>}
+              </div>
+            </div>
+    );
+}
